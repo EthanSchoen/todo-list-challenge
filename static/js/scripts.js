88323176a973e58b1a9de93300c5b0d481ca40d0
EthanSchoen <ethascho@gmail.com>
2020-11-04 14:46:24 -0500

    edit button working now

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -19,18 +19,21 @@ function addTask(taskName) {
     var operations = document.createElement("td");
     operations.setAttribute("class", "opcolumn");
 // delete button
-    var deleteButton = document.createElement("input");
-    deleteButton.setAttribute("type", "button");
-    deleteButton.setAttribute("value", "Remove");
-    deleteButton.setAttribute("onclick", "deleteRow("+UID+")");
+    var removeButton = document.createElement("input");
+    removeButton.setAttribute("type", "button");
+    removeButton.setAttribute("value", "Remove");
+    removeButton.setAttribute("class", "removeButton");
+    removeButton.setAttribute("onclick", "deleteRow("+UID+")");
 // edit button
     var editButton = document.createElement("input");
     editButton.setAttribute("type", "button");
     editButton.setAttribute("value", "Edit");
+    editButton.setAttribute("class", "editButton");
     editButton.setAttribute("onclick", "editRow("+UID+")");
 // finish up row
-    operations.appendChild(deleteButton);
     operations.appendChild(editButton);
+    operations.innerHTML += " ";
+    operations.appendChild(removeButton);
     row.appendChild(operations);
 
 // Clear input field
@@ -38,14 +41,47 @@ function addTask(taskName) {
 }
 
 function editRow(rowNumber) {
-    document.getElementById(rowNumber).getElementsByClassName("taskcolumn")[0].innerHTML = "edited";
+    var task = document.getElementById(rowNumber).getElementsByClassName("taskcolumn")[0];
+    var oldTask = task.innerHTML;
+    task.innerHTML = "";
+
+// replace task with edit field for user to make changes
+    var editField = document.createElement("input");
+    editField.setAttribute("value", oldTask);
+    editField.setAttribute("class", "editTaskInput");
+    task.appendChild(editField);
+
+// Switch edit button to done button
+    var button = document.getElementById(rowNumber).getElementsByClassName("editButton")[0];
+    button.setAttribute("class", "doneButton");
+    button.setAttribute("onclick", "editDone("+rowNumber+")");
+    button.setAttribute("value", "Done");
+}
+
+function editDone(rowNumber) {
+    var task = document.getElementById(rowNumber).getElementsByClassName("taskcolumn")[0];
+// set html to input
+    task.innerHTML = task.getElementsByClassName("editTaskInput")[0].value
+
+// switch button from done back to edit
+    var button = document.getElementById(rowNumber).getElementsByClassName("doneButton")[0];
+    button.setAttribute("class", "editButton");
+    button.setAttribute("onclick", "editRow("+rowNumber+")");
+    button.setAttribute("value", "Edit");
 }
 
 function deleteRow(rowNumber){
     document.getElementById(rowNumber).remove();
 }
 
-function lookforenter(ele) {
+function editTaskEnter(ele) {
+    if(event.key === 'Enter') {
+        event.preventDefault();
+        editDone();
+    }
+}
+
+function newTaskEnter(ele) {
     if(event.key === 'Enter') {
         event.preventDefault();
         addTask(document.getElementById('newtask').value);
