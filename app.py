fd6482ab3055a7c3c3aa08d76b7ad18dbd0c14fe
EthanSchoen <ethascho@gmail.com>
2020-11-05 13:44:47 -0500

    begining of adding users

--- app.py
+++ app.py
@@ -1,20 +1,36 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_user import login_required, UserManager, UserMixin, SQLAlchemyAdapter
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'thisisasecretkeyiguess'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/todo.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'False'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['CSRF_ENABLED'] = True
+app.config['USER_ENABLE_EMAIL'] = False
 db = SQLAlchemy(app)
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False, unique=False)
+    password = db.Column(db.String(255), nullable=False, unique=False, server_default='')
+    active = db.Column(db.Boolean(), nullable=False, server_default='0')
+
 
 class Tasks(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.String(50), nullable=False, unique=False)
     task = db.Column(db.String(200), unique=False, nullable=False)
     complete = db.Column(db.Boolean, unique=False, default=False)
 
     def __repr__(self):
         return '<Task %r>' % self.task
 
+db_adapter = SQLAlchemyAdapter(db, User)
+user_manager = UserManager(db_adapter, app)
+
 @app.route('/', methods=['GET'])
+@login_required
 def index():
     taskList = Tasks.query.all()
     return render_template('index.html.j2', tasks=taskList)
