1e0a0e82481a23980ebefc457779dc2d0c0bdcc4
EthanSchoen <ethascho@gmail.com>
2020-11-05 15:32:32 -0500

    Users seem to be working now

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_user import login_required, UserManager, UserMixin, SQLAlchemyAdapter
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'thisisasecretkeyiguess'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/todo.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/users.db'
+app.config['SQLALCHEMY_BINDS'] = { 'todo' : 'sqlite:///data/todo.db' }
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['CSRF_ENABLED'] = True
 app.config['USER_ENABLE_EMAIL'] = False
@@ -12,14 +13,15 @@ db = SQLAlchemy(app)
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), nullable=False, unique=False)
+    username = db.Column(db.String(50), nullable=False, unique=True)
     password = db.Column(db.String(255), nullable=False, unique=False, server_default='')
     active = db.Column(db.Boolean(), nullable=False, server_default='0')
-
+    userTasks = db.relationship('Tasks', backref='user', lazy=True)
 
 class Tasks(db.Model):
+    __bind_key__ = 'todo'
     id = db.Column(db.Integer, primary_key=True)
-    user = db.Column(db.String(50), nullable=False, unique=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     task = db.Column(db.String(200), unique=False, nullable=False)
     complete = db.Column(db.Boolean, unique=False, default=False)
 
@@ -32,13 +34,14 @@ user_manager = UserManager(db_adapter, app)
 @app.route('/', methods=['GET'])
 @login_required
 def index():
-    taskList = Tasks.query.all()
-    return render_template('index.html.j2', tasks=taskList)
+    # taskList = Tasks.query.all()
+    taskList = Tasks.query.filter_by(user_id=session['_user_id']).all()
+    return render_template('index.html.j2', tasks=taskList, userid=session['_user_id'])
 
 @app.route('/add', methods=['POST'])
 def add():
     if not request.form['newtask'].strip() == '':
-        newTask = Tasks(task=request.form['newtask'])
+        newTask = Tasks(user_id=session['_user_id'], task=request.form['newtask'])
         db.session.add(newTask)
         db.session.commit()
     return redirect(url_for('index'))
